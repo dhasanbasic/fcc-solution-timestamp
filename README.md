a114ab3faa1429e369a1b186c4803b8f3e9ff480
Dinko Hasanbasic <dinko.hasanbasic@gmail.com>
2022-03-26 20:35:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# [Timestamp Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice)
+
+Solution to the timestamp microservice challenge.
+
+To test locally:
+
+    npm run serve
+
+Then you should be able access the application via http://localhost:3000/ .
---
+++ server.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, () => {
+  console.log('⚡️[server]: Your app is listening on port ' + port);
+});
